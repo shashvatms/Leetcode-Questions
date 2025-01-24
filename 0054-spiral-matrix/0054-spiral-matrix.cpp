d6f2eaab8cb06d11c1c44645d6cc8132be299e28
shashvatms <119059021+shashvatms@users.noreply.github.com>
2025-01-24 13:30:49 +0530

    Time: 0 ms (100%), Space: 9.3 MB (77.73%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix[0].size() - 1;
-        int m = matrix.size() - 1;
-        int top = 0;
-        int right = n;
-        int left = 0;
-        int bottom = m;
-
         vector<int> res;
-
-        while(top <= bottom && left <= right){
-            // Top row
-            for(int i = left; i <= right; i++){
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int left = 0;
+        int right =  m-1;
+        int top = 0;
+        int bottom = n-1;
+        while(left<=right&&top<=bottom){
+            for(int i = left;i<=right;i++){
                 res.push_back(matrix[top][i]);
             }
             top++;
-
-            // Right column
-            for(int i = top; i <= bottom; i++){
+            for(int i=top;i<=bottom;i++){
                 res.push_back(matrix[i][right]);
             }
             right--;
-
-            if(top <= bottom){
-                // Bottom row
-                for(int i = right; i >= left; i--){
-                    res.push_back(matrix[bottom][i]);
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                res.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
-
-            if(left <= right){
-                // Left column
-                for(int i = bottom; i >= top; i--){
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
                     res.push_back(matrix[i][left]);
                 }
                 left++;
