cad191f0da0db4be78daf4b22797ff978b02f97c
shashvatms <119059021+shashvatms@users.noreply.github.com>
2025-01-25 10:22:16 +0530

    Time: 0 ms (100%), Space: 19.8 MB (84.08%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,44 +3,61 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
         int n = nums.size();
-        int count1=0;
-        int count2=0;
-        int c1=0,c2=0;
-        for(int num: nums){
-            if(num==c1){
-                count1++;
-            }
-            else if(num==c2){
-                count2++;
-            }
-            else if(count1==0){
-                c1=num;
-                count1++;
-            }
-            else if(count2==0){
-                c2=num;
-                count2++;
+        // int count1=0;
+        // int count2=0;
+        // int c1=0,c2=0;
+        // for(int num: nums){
+        //     if(num==c1){
+        //         count1++;
+        //     }
+        //     else if(num==c2){
+        //         count2++;
+        //     }
+        //     else if(count1==0){
+        //         c1=num;
+        //         count1++;
+        //     }
+        //     else if(count2==0){
+        //         c2=num;
+        //         count2++;
+        //     }
+        //     else{
+        //         count1--;
+        //         count2--;
+        //     }
+        // }
+        // count1=0;
+        // count2=0;
+        // for(int num: nums){
+        //     if(num==c1){
+        //         count1++;
+        //     }
+        //     else if(num==c2){
+        //         count2++;
+        //     }
+        // }
+        // if(count1>n/3){
+        //     ans.push_back(c1);
+        // }
+        // if(count2>n/3){
+        //     ans.push_back(c2);
+        // }
+        // return ans;
+        sort(nums.begin(),nums.end());
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]){
+                count++;
             }
             else{
-                count1--;
-                count2--;
+                if(count>n/3){
+                    ans.push_back(nums[i-1]);
+                }
+                count=1;
             }
         }
-        count1=0;
-        count2=0;
-        for(int num: nums){
-            if(num==c1){
-                count1++;
-            }
-            else if(num==c2){
-                count2++;
-            }
-        }
-        if(count1>n/3){
-            ans.push_back(c1);
-        }
-        if(count2>n/3){
-            ans.push_back(c2);
+        if(count>n/3){
+            ans.push_back(nums[n-1]);
         }
         return ans;
     }
