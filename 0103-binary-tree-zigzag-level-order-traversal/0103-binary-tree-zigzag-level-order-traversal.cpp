7c5b4996baaf7667b34cd84ff794370de4c35dc9
shashvatms <119059021+shashvatms@users.noreply.github.com>
2025-02-14 22:45:03 +0530

    Time: 2 ms (13.29%), Space: 15.3 MB (5.93%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,71 +12,36 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        // vector<vector<int>> result;
-        // if(!root){
-        //     return result;
-        // }
-        // queue<TreeNode*> q;
-        // q.push(root);
-        // bool flag = true;
-        // while(!q.empty()){
-        //     deque<int> temp;
-        //     int s = q.size();
-        //     for(int i=0;i<s;i++){
-        //         TreeNode* curr = q.front();
-        //         q.pop();
-        //         if(flag){
-        //             temp.push_back(curr->val);
-        //         }
-        //         else{
-        //             temp.push_front(curr->val);
-        //         }
-        //         if(curr->left){
-        //             q.push(curr->left);
-        //         }
-        //         if(curr->right){
-        //             q.push(curr->right);
-        //         }
-        //     }
-        //     flag!=flag;
-        //     result.push_back(vector<int>(temp.begin(),temp.end()));
-        // }
-        // return result;
         vector<vector<int>> result;
-        if (!root) {
+        if(!root){
             return result;
         }
-
         queue<TreeNode*> q;
         q.push(root);
-        bool flag = true;  // true -> left to right, false -> right to left
-
-        while (!q.empty()) {
-            int size = q.size();
-            deque<int> temp;  // Use deque to insert elements at front or back
-
-            for (int i = 0; i < size; i++) {
+        bool flag = true;
+        while(!q.empty()){
+            deque<int> temp;
+            int s = q.size();
+            for(int i=0;i<s;i++){
                 TreeNode* curr = q.front();
                 q.pop();
-                
-                if (flag) {
-                    temp.push_back(curr->val);  // Insert normally
-                } else {
-                    temp.push_front(curr->val); // Insert in reverse order
+                if(flag){
+                    temp.push_back(curr->val);
                 }
-
-                if (curr->left) {
+                else{
+                    temp.push_front(curr->val);
+                }
+                if(curr->left){
                     q.push(curr->left);
                 }
-                if (curr->right) {
+                if(curr->right){
                     q.push(curr->right);
                 }
             }
-
-            result.push_back(vector<int>(temp.begin(), temp.end()));
-            flag = !flag;  // Toggle flag for the next level
+            
+            result.push_back(vector<int>(temp.begin(),temp.end()));
+            flag=!flag;
         }
         return result;
-
     }
 };
