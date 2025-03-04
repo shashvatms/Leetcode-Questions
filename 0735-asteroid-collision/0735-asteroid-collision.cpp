95a61b45d22edbba020a4e538da3f869e45beaef
shashvatms <119059021+shashvatms@users.noreply.github.com>
2025-03-04 23:38:30 +0530

    Time: 0 ms (100%), Space: 21.5 MB (96.59%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,40 +1,25 @@
-// class Solution {
-// public:
-//     vector<int> asteroidCollision(vector<int>& asteroids) {
-//         vector<int> temp;
-//         for(int i : asteroids){
-//             while(!temp.empty() && temp.back() > 0 && i < 0){
-//                 if(abs(temp.back()) < abs(i)){
-//                     temp.pop_back();
-//                     continue;
-//                 }
-//                 else if(abs(temp.back())==abs(i)){
-//                     temp.pop_back();
-//                 }
-//             }
-//         }
-//     }
-// };
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> temp;
-        for (int asteroid : asteroids) {
-            bool destroyed = false;
-            while (!temp.empty() && temp.back() > 0 && asteroid < 0) {
-                if (abs(temp.back()) < abs(asteroid)) {
-                    temp.pop_back();  // Right-moving asteroid is destroyed
+        for(int i : asteroids){
+            bool d = false;
+            while(!temp.empty() && temp.back() > 0 && i < 0){
+                if(abs(temp.back()) < abs(i)){
+                    temp.pop_back();
                     continue;
-                } else if (abs(temp.back()) == abs(asteroid)) {
-                    temp.pop_back();  // Both asteroids are destroyed
                 }
-                destroyed = true;
+                else if(abs(temp.back())==abs(i)){
+                    temp.pop_back();
+                }
+                d = true;
                 break;
             }
-            if (!destroyed) {
-                temp.push_back(asteroid);
+            if(!d){
+                temp.push_back(i);
             }
         }
         return temp;
     }
 };
+
