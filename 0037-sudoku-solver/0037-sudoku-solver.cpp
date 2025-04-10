86896857873f6fdd20bb95631fa81b17eefc6eca
shashvatms <119059021+shashvatms@users.noreply.github.com>
2025-04-10 13:30:20 +0530

    Time: 379 ms (48.01%), Space: 8.6 MB (99.23%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -8,32 +8,35 @@ public:
     }
 private:
     bool solve(vector<vector<char>>& board) {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] == '.') {
-                    for (char digit = '1'; digit <= '9'; digit++) {
-                        if (isValid(board, row, col, digit)) {
-                            board[row][col] = digit;
-                            if (solve(board))
-                                return true;
-                            board[row][col] = '.'; 
+       for(int row=0;row<9;row++){
+            for(int col = 0;col<9;col++){
+              if(board[row][col]=='.'){
+                    for(char digit='1';digit<='9';digit++){
+                        if(isvalid(board,row,col,digit)){
+                              board[row][col] = digit;
+                              if(solve(board)){
+                                  return true;
+                              } 
+                              board[row][col] = '.';
                         }
                     }
-                    return false; 
-                }
+                    return false;
+                }      
             }
-        }
-        return true; 
+       }
+       return true;
     }
 
-    bool isValid(vector<vector<char>>& board, int row, int col, char digit) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == digit || board[i][col] == digit)
+    bool isvalid(vector<vector<char>>& board, int row, int col, char digit) {
+        for(int i=0;i<9;i++){
+            if(board[row][i]==digit || board[i][col]==digit){
                 return false;
-            int boxRow = 3 * (row / 3) + i / 3;
-            int boxCol = 3 * (col / 3) + i % 3;
-            if (board[boxRow][boxCol] == digit)
+            }
+            int boxrow = 3*(row/3) + (i/3);
+            int boxcol = 3*(col/3) + (i%3);
+            if(board[boxrow][boxcol]==digit){
                 return false;
+            }
         }
         return true;
     }
