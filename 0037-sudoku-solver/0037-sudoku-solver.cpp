942afeede18e32651f610e8620d1ce3f13796922
shashvatms <119059021+shashvatms@users.noreply.github.com>
2025-04-10 11:26:32 +0530

    Time: 373 ms (52.02%), Space: 8.6 MB (99.23%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+private:
+    bool solve(vector<vector<char>>& board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char digit = '1'; digit <= '9'; digit++) {
+                        if (isValid(board, row, col, digit)) {
+                            board[row][col] = digit;
+                            if (solve(board))
+                                return true;
+                            board[row][col] = '.'; 
+                        }
+                    }
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char digit) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == digit || board[i][col] == digit)
+                return false;
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+            if (board[boxRow][boxCol] == digit)
+                return false;
+        }
+        return true;
+    }
+};
