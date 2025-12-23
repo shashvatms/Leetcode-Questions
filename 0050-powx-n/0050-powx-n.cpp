b5d5c84d0cc6687579916eaa290ff86c799be0b8
shashvatms <119059021+shashvatms@users.noreply.github.com>
2025-12-23 20:03:43 +0530

    Time: 0 ms (100%), Space: 8.5 MB (91.82%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -7,11 +7,26 @@ public:
         return half*half*x;
     }
     double myPow(double x, int n) {
+        // long long p = n;
+        // if(p<0){
+        //     x = 1/x;
+        //     return power(x,-p);
+        // }
+        // return power(x,p);
         long long p = n;
-        if(p<0){
+        if(n<0){
+            p = -p;
             x = 1/x;
-            return power(x,-p);
         }
-        return power(x,p);
+        double result = 1;
+        double current = x;
+        while(p>0){
+            if(p%2==1){
+                result = result * current;
+            }
+            current = current * current;
+            p = p/2;
+        }
+        return result;
     }
 };
