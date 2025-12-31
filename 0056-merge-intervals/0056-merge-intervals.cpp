b64666ae9e832da3af970d1cda17a16b7d4d0e36
shashvatms <119059021+shashvatms@users.noreply.github.com>
2025-12-31 19:07:50 +0530

    Time: 7 ms (42.56%), Space: 23.7 MB (93.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,8 +3,8 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
-        for(auto &i: intervals){
-            if(ans.empty() || ans.back()[1] < i[0]){
+        for(auto &i : intervals){
+            if(ans.empty() || ans.back()[1]<i[0]){
                 ans.push_back(i);
             }
             else{
