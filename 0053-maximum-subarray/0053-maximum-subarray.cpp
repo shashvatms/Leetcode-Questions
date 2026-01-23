6508c67db486cce115cfca769cefb52586333762
shashvatms <119059021+shashvatms@users.noreply.github.com>
2026-01-23 18:10:38 +0530

    Time: 0 ms (100%), Space: 71.8 MB (51.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,9 +3,9 @@ public:
     int maxSubArray(vector<int>& nums) {
         int currsum = nums[0];
         int maxsum = nums[0];
-        for(int i = 1;i<nums.size();i++){
+        for(int i=1;i<nums.size();i++){
             currsum = max(nums[i],currsum+nums[i]);
-            maxsum = max(currsum,maxsum);
+            maxsum = max(maxsum,currsum);
         }
         return maxsum;
     }
