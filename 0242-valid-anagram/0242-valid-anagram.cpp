d81c91a3133e16de0c9df39ee6a8ce132db0ae80
shashvatms <119059021+shashvatms@users.noreply.github.com>
2026-02-18 00:26:11 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,12 +5,12 @@ public:
             return false;
         }
         vector<int> count(26,0);
-        for(int i=0;i<s.length();i++){
-            count[s[i] - 'a']++;
-            count[t[i] - 'a']--;
+        for(int i=0;i<s.size();i++){
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
         }
-        for(int c: count){
-            if(c!=0){
+        for(int i: count){
+            if(i!=0){
                 return false;
             }
         }
