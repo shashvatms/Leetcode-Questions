ccc01ee0175b001723e0f427a9eae7d2f1f9db81
shashvatms <119059021+shashvatms@users.noreply.github.com>
2026-02-18 17:53:02 +0530

    Time: 7 ms (5.08%), Space: 12.3 MB (7.02%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
+        string st;
         int k = part.size();
-        
-        for(int i = 0; i <= (int)s.size() - k;){
-            bool found = true;
-            for (int j = 0; j < k; j++) {
-                if(s[i+j]!=part[j]){
-                    found = false;
-                    break;
-                }
-            }
-            if(found){
-                s.erase(i,k);
-                i = max(0,i-k);
-            }
-            else{
-                i++;
+        for(char c : s){
+            st.push_back(c);
+            if(st.size()>=k && st.substr(st.size()-k)==part){
+                st.erase(st.size()-k);
             }
         }
-        return s;
+        return st;
     }
 };
