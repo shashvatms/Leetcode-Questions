31eaaf265288b03b565809056945511bc63f44f1
shashvatms <119059021+shashvatms@users.noreply.github.com>
2026-02-18 17:37:30 +0530

    Time: 0 ms (100%), Space: 9.2 MB (91.79%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        // int k = part.size();
-        while(true){
-            int pos = s.find(part);
-            if(pos==string::npos){
-                break;
+        int k = part.size();
+        
+        for(int i = 0; i <= (int)s.size() - k;){
+            bool found = true;
+            for (int j = 0; j < k; j++) {
+                if(s[i+j]!=part[j]){
+                    found = false;
+                    break;
+                }
             }
-            s.erase(pos,part.size());
+            if(found){
+                s.erase(i,k);
+                i = max(0,i-k);
+            }
+            else{
+                i++;
+            }
         }
         return s;
-        // for(int i=0;i<s.size();i++){
-        //     bool found = true;
-        //     int j = i;
-        //     for (int x = 0; x < part.size(); x++, j++) {
-        //         if(s[j]!=part[x]){
-        //             found = false;
-        //             break;
-        //         }
-        //     }
-        //     if(found){
-        //         s.erase(i,k);
-        //     }
-        // }
-        // return s;
     }
 };
